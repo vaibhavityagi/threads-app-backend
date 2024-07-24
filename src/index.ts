11bdba978c91b696201e771561fdac497485e444
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-07-24 13:29:08 +0530

    gql server setup

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
+
+  app.use(express.json());
+
+  // graph ql server
+  const gqlServer = new ApolloServer({
+    typeDefs: `type Query {
+    hello: String}`,
+    resolvers: {
+      Query: {
+        hello: () => `I am a graphql server`,
+      },
+    },
+  });
+  // starting the gql server
+  await gqlServer.start();
+
+  //   gql server port
+  app.use("/graphql", expressMiddleware(gqlServer));
+
+  // http server
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Server is healthy",
+    });
+  });
+
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+}
+
+init();
+// a global level async fn is created so that await call to the gqlServer can be made
